66a706e0825ad185a9fe92a0c2f07ff20a37045b
user <user@asd21>
2020-06-16 09:35:54 +0900

    pr0616_merge_test

---
+++ 30224/src/pr0616_newtest.java
@@ -0,0 +1,11 @@
+
+public class pr0616_newtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new test");
+		System.out.println("test_0616");
+
+	}
+
+}
